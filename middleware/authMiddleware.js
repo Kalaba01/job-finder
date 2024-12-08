201105ffa162a145b04e1f494318a54518ff47d3
Kalaba <edinkalaba45@gmail.com>
2024-12-08 19:16:49 +0100

    Dodat 403 page

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,19 +9,19 @@ exports.isAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.role === "admin") {
     return next();
   }
-  res.status(403).send("Access Denied: Admins Only");
+  res.status(403).render("403", { locale: req.getLocale() });
 };
 
 exports.isFirm = (req, res, next) => {
   if (req.isAuthenticated() && req.user.role === "firm") {
     return next();
   }
-  res.status(403).send("Access Denied: Firms Only");
+  res.status(403).render("403", { locale: req.getLocale() });
 };
 
 exports.isCandidate = (req, res, next) => {
   if (req.isAuthenticated() && req.user.role === "candidate") {
     return next();
   }
-  res.status(403).send("Access Denied: Candidates Only");
+  res.status(403).render("403", { locale: req.getLocale() });
 };
