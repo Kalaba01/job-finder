7543109be36116bee2470aa70eb00028f152f76c
Kalaba <edinkalaba45@gmail.com>
2024-12-18 14:21:05 +0100

    Dodatno zasticena ruta za prikazivanje slika

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const imageController = require("../controllers/imageController");
 const router = express.Router();
+const { authMiddleware } = require("../middleware");
 
 // Endpoint za prikazivanje slike na osnovu ID-a
-router.get("/:id", imageController.getImageById);
+router.get("/:id", imageController.getImageById, authMiddleware.isAuthenticated);
 
 module.exports = router;
